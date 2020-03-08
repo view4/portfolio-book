a8d625ec3e234c2aead9d68acec3a7e3473dfadd
Gabriel Morris <morrisgaby987@googlemail.com>
2020-03-08 22:04:44 +0200

    Updates

---
+++ precache-manifest.5836d041c4ae21bf83381614dbb00534.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "856f7bfc1f66e6c6cce5a289f681f3b7",
+    "url": "/portfolio-book/index.html"
+  },
+  {
+    "revision": "60ca4ae657e7c09a8308",
+    "url": "/portfolio-book/static/css/main.bb89cac1.chunk.css"
+  },
+  {
+    "revision": "8ee78e0e5d8a201a75f7",
+    "url": "/portfolio-book/static/js/2.f31b4536.chunk.js"
+  },
+  {
+    "revision": "333cdd120ec335387dba84a5da98ccd5",
+    "url": "/portfolio-book/static/js/2.f31b4536.chunk.js.LICENSE"
+  },
+  {
+    "revision": "60ca4ae657e7c09a8308",
+    "url": "/portfolio-book/static/js/main.0cdcd9e4.chunk.js"
+  },
+  {
+    "revision": "b6dd612a6f30b0af3e16",
+    "url": "/portfolio-book/static/js/runtime-main.bdfddc25.js"
+  },
+  {
+    "revision": "5cf1b5f8b3cd1f23d3f8dc06cdb3b9a1",
+    "url": "/portfolio-book/static/media/Benj+sushi.5cf1b5f8.jpg"
+  },
+  {
+    "revision": "441aa419ddd6d8836d5cb2526261fec6",
+    "url": "/portfolio-book/static/media/Family.441aa419.jpg"
+  },
+  {
+    "revision": "3b8e254586e7224225e4a7c8b29aa5fb",
+    "url": "/portfolio-book/static/media/Nate+I.3b8e2545.jpg"
+  },
+  {
+    "revision": "9a4d6b37ed75192011ae235a18ac057b",
+    "url": "/portfolio-book/static/media/The-Portfolio.9a4d6b37.png"
+  },
+  {
+    "revision": "1afa068b741e042f93c0e5d8576fb305",
+    "url": "/portfolio-book/static/media/Token.1afa068b.jpg"
+  },
+  {
+    "revision": "f1dfcdfccc7385a415630b88d401b562",
+    "url": "/portfolio-book/static/media/Tzahi_and_I.f1dfcdfc.jpg"
+  },
+  {
+    "revision": "0058f12ecb7bcdb2f618492e8efc2d98",
+    "url": "/portfolio-book/static/media/apple-three.0058f12e.png"
+  },
+  {
+    "revision": "8562e8946b20d2a1cac2fee36ba5b61b",
+    "url": "/portfolio-book/static/media/apple-two.8562e894.png"
+  },
+  {
+    "revision": "a71011b807ab99074ae77c65a2251500",
+    "url": "/portfolio-book/static/media/apple.a71011b8.png"
+  },
+  {
+    "revision": "e2573dedb560fc5934df8e9c01c1bf07",
+    "url": "/portfolio-book/static/media/click-me.e2573ded.png"
+  },
+  {
+    "revision": "4486f8ac167988b0c268ed546e041971",
+    "url": "/portfolio-book/static/media/fleetonomy-three.4486f8ac.png"
+  },
+  {
+    "revision": "437e7088e2062a744332dd1a66cab1bd",
+    "url": "/portfolio-book/static/media/fleetonomy-two.437e7088.png"
+  },
+  {
+    "revision": "519133b3f8b2aa8fc6a545ea6c3e4f72",
+    "url": "/portfolio-book/static/media/hob-one.519133b3.png"
+  },
+  {
+    "revision": "5ad7e5bbef883c77cde79668c12ff221",
+    "url": "/portfolio-book/static/media/hob-two.5ad7e5bb.png"
+  },
+  {
+    "revision": "a7762eaee0a27e4c0c7943429bd3ab37",
+    "url": "/portfolio-book/static/media/orderPlease-one.a7762eae.png"
+  },
+  {
+    "revision": "addb299ed6434946b66e40510f4fb63b",
+    "url": "/portfolio-book/static/media/orderPlease-three.addb299e.png"
+  },
+  {
+    "revision": "c65428477b19c268dd6765b997f09f77",
+    "url": "/portfolio-book/static/media/orderPlease-two.c6542847.png"
+  },
+  {
+    "revision": "7ae04854179c284bb7037fd4367173f8",
+    "url": "/portfolio-book/static/media/petwell-four.7ae04854.png"
+  },
+  {
+    "revision": "4fbafa2b051125ba2eccc8874ff71565",
+    "url": "/portfolio-book/static/media/petwell-three.4fbafa2b.png"
+  },
+  {
+    "revision": "a7e330a92510ebbac121f5ed4be65725",
+    "url": "/portfolio-book/static/media/petwell-two.a7e330a9.png"
+  },
+  {
+    "revision": "1d09b670268aca767a720dc5134ff1f4",
+    "url": "/portfolio-book/static/media/plain.1d09b670.jpg"
+  },
+  {
+    "revision": "adf0902f979fa557911eee7a70c6cbae",
+    "url": "/portfolio-book/static/media/todo-one.adf0902f.png"
+  },
+  {
+    "revision": "9fc166f344ead524cc4fe6a945d2f72a",
+    "url": "/portfolio-book/static/media/todo-three.9fc166f3.png"
+  },
+  {
+    "revision": "af8e4895493297e07c776c35a03fbcfa",
+    "url": "/portfolio-book/static/media/todo-two.af8e4895.png"
+  },
+  {
+    "revision": "ad4912136d29d5a567b3f94c719060b5",
+    "url": "/portfolio-book/static/media/troox-one.ad491213.png"
+  },
+  {
+    "revision": "d3a3c3bf4b4c9831bb42481ea5105886",
+    "url": "/portfolio-book/static/media/troox-three.d3a3c3bf.png"
+  },
+  {
+    "revision": "767f526e922c4cb6d36aa5258ea14ac5",
+    "url": "/portfolio-book/static/media/troox-two.767f526e.png"
+  },
+  {
+    "revision": "bd19bd46062d6eac8ec29f33c6778588",
+    "url": "/portfolio-book/static/media/tv-one.bd19bd46.png"
+  },
+  {
+    "revision": "f9eb0d3d03acb81bd595b31e8c264db4",
+    "url": "/portfolio-book/static/media/tv-three.f9eb0d3d.png"
+  },
+  {
+    "revision": "5b45cfb038bd0f1df9daf62272730124",
+    "url": "/portfolio-book/static/media/tv-two.5b45cfb0.png"
+  }
+]);
